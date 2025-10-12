dfa3905ad9b2a4adb106536ec3d8d2eb4da080f4
Adham-Elsaygh <adham.elsaygh@stud.hshl.de>
2025-10-12 22:51:14 +0200

    Update TrafficLight.ino

--- TrafficLight.ino
+++ TrafficLight.ino
@@ -1,52 +1,97 @@
-//Pin Definitions
-const int  green_pin = 11;
-const int  yellow_pin = 12;
-const int red_pin = 13;
-//Color Runing Time (ms)
-const long green_time = 20000;
-const long yellow_time = 5000;
-const long red_time = 20000;
-//Trafficlight States
-enum TrafficLightState {green_state, yellow_state, red_state};
-int CurrentState = green_state;
-long previousMillis = 0;
+#include <arduino.h>
 
-void setup(){
-  pinMode(green_pin, OUTPUT);
-  pinMode(yellow_pin, OUTPUT);
-  pinMode(red_pin, OUTPUT);
+class TrafficLight {
+//main program only interacts with begin() and update()
+  private:
+    int greenPin, yellowPin, redPin;
+    long greenTime, yellowTime, redTime;
+    long previousMillis;
+//traffic light states
+    enum State {GREEN, YELLOW, RED};
+    State currentState;
+    State previousState;
 
-  digitalWrite(green_pin, HIGH);
-  previousMillis = millis();
+  public:
+    // every traffic light colour duration can be set indvidually
+  // pin definitons can be set individually
+    TrafficLight(int g, int y, int r, long gTime, long yTime, long rTime) {
+      greenPin = g;
+      yellowPin = y;
+      redPin = r;
+      greenTime = gTime;
+      yellowTime = yTime;
+      redTime = rTime;
+      currentState = GREEN;
+      previousState = RED;  // So first yellow knows what came before
+      previousMillis = 0;
+    }
+
+    void begin() {
+      pinMode(greenPin, OUTPUT);
+      pinMode(yellowPin, OUTPUT);
+      pinMode(redPin, OUTPUT);
+      turnOn(greenPin);
+      previousMillis = millis();
+    }
+
+    void update() {
+      long currentMillis = millis();
+//after red or green defaults to yellow,but after yellow checks previous state and act accordingly
+      switch (currentState) {
+        case GREEN:
+          if (currentMillis - previousMillis >= greenTime) {
+            switchTo(YELLOW);
+          }
+          break;
+
+        case RED:
+          if (currentMillis - previousMillis >= redTime) {
+            switchTo(YELLOW);
+          }
+          break;
+
+        case YELLOW:
+          if (currentMillis - previousMillis >= yellowTime) {
+            if (previousState == GREEN) switchTo(RED);
+            else if (previousState == RED) switchTo(GREEN);
+          }
+          break;
+      }
+    }
+
+  private:
+    void switchTo(State nextState) {
+      // Turn off all lights first
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(redPin, LOW);
+
+      previousState = currentState;
+      currentState = nextState;
+      previousMillis = millis();
+
+      // Turn on the LED for the new state
+      switch (nextState) {
+        case GREEN:  turnOn(greenPin);  break;
+        case YELLOW: turnOn(yellowPin); break;
+        case RED:    turnOn(redPin);    break;
+      }
+    }
+//turn on function
+    void turnOn(int pin) {
+      digitalWrite(pin, HIGH);
+    }
+};
+//START
+TrafficLight light(11, 12, 13, 20000, 5000, 20000);
+
+void setup() {
+  light.begin();
 }
 
-void loop(){
-  long currentMillis= millis();
-
-  switch(CurrentState){
-    case green_state:
-      if(currentMillis-previousMillis >= green_time){
-        previousMillis = currentMillis;
-        digitalWrite(green_pin, LOW);
-        digitalWrite(yellow_pin, HIGH);
-        CurrentState = yellow_state;
-      }
-      break;
-    case yellow_state:
-      if(currentMillis-previousMillis >= yellow_time){
-        previousMillis = currentMillis;
-        digitalWrite(yellow_pin, LOW);
-        digitalWrite(red_pin, HIGH);
-        CurrentState = red_state;
-      }
-      break;
-    case red_state:
-      if(currentMillis-previousMillis >= red_time){
-        previousMillis = currentMillis;
-        digitalWrite(red_pin, LOW);
-        digitalWrite(green_pin, HIGH);
-        CurrentState = green_state;
-      }
-      break;
+void loop() {
+  light.update();
+}
   } 
+
 }
